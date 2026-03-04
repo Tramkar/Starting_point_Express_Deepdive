348b5e4e6682d72c3961565c77b0707c30dd8fc1
Alexey <20alexey02@gmail.com>
2026-03-04 14:27:14 +0000

    changes made for step002

--- README.md
+++ README.md
@@ -14,3 +14,12 @@ above</p>
 <p>For the next step, create and switch to step002. Use this incremental approach so 
 changes are contained in incremental branches. This makes it easy to revert back to 
 any of the stages by switching to the target branch.</p>
+
+<h1>ExpressJS Deep Dive</h1> 
+<h2>Tasks done in step 2</h2> 
+<ul> 
+<li>default endpoint "/" displays message in json format</li> 
+<li>"api/users" endpoint displays the users</li> 
+<li>"api/products" endpoint displays the products</li> 
+<li>Branch "step002" now exists in GitHub repo</li> 
+<p>Now create and switch to "step003" branch</p> 
--- src/index.mjs
+++ src/index.mjs
@@ -8,3 +8,25 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => { 
     console.log(`Server is running on port ${PORT}`); 
 });
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: "hey!This is a test message."});
+});
+
+app.get('/api/user',(req, res) => {
+    const user = [
+    { id: 1, name: 'Alice'},
+    { id: 2, name: 'Bob'},
+    { id: 3, name: 'Charlie'}
+];
+response.status(200).json(user);
+});
+
+app.get('/api/product',(req, res) => {
+    const product = [
+    { id: 1, name: 'Laptop', price: 999.99},
+    { id: 2, name: 'Mouse', price: 29.99},
+    { id: 3, name: 'Keyboard', price: 79.99}
+];
+response.status(200).json(product);
+});
